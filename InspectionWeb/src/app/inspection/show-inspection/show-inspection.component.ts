29918d4fb23e163f85262e585f7423b6fb63df81
Mahm00dSharif <sharif.mahmoud@outlook.com>
2022-06-18 03:14:24 +0300

    lists

--- InspectionWeb/src/app/inspection/show-inspection/show-inspection.component.ts
+++ InspectionWeb/src/app/inspection/show-inspection/show-inspection.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { InspectionApiService } from 'src/app/inspection-api.service';
+import { Inspection } from 'src/app/Models/Inspection.class';
+import { InspectionType } from 'src/app/Models/InspectionType.class';
 @Component({
   selector: 'app-show-inspection',
   templateUrl: './show-inspection.component.html',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowInspectionComponent implements OnInit {
 
-  constructor() { }
+  InspectionList!: Observable<Inspection[]>;
+  InspectionTypesList!:Observable<InspectionType[]>;
+  InspectionTypeList:any = [];
+
+  constructor(private service:InspectionApiService) { }
 
   ngOnInit(): void {
+    this.InspectionList = this.service.GetInspectionsList();
   }
 
 }
